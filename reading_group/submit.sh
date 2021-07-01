41a51f2ef1facb7610c57600f38aed2d3ddefa4d
yuxinchen2017 <threshold.vincent@gmail.com>
2021-07-01 13:58:37 -0400

    First commit

---
+++ reading_group/submit.sh
@@ -0,0 +1,15 @@
+python jemdoc.py index.jemdoc
+python jemdoc.py Publications.jemdoc
+python jemdoc.py Teaching.jemdoc
+python jemdoc.py Others.jemdoc
+python jemdoc.py Research.jemdoc
+python jemdoc.py Education.jemdoc
+python jemdoc.py InvitedTalk.jemdoc
+python jemdoc.py travel.jemdoc
+python jemdoc.py Group.jemdoc
+python jemdoc.py Publications_year.jemdoc
+python jemdoc.py Software.jemdoc
+python jemdoc.py Workshops.jemdoc
+
+
+scp Publications.html index.html InvitedTalk.html Group.html Education.html Teaching.html Research.html travel.html Workshops.html MENU Publications_year.html Software.html jemdoc.css ../../My\ Documents/Resume/Resume_Yuxin_CHEN.pdf yc5@arizona.princeton.edu:public_html/
